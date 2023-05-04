df4786f5be0aea5d9ce2b32113c078bfb516de96
linbaixiang <602899006@qq.com>
2023-05-04 17:46:28 +0800

    update 增加py程序头

--- dailyma.py
+++ dailyma.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+# !/usr/bin/python3
 
 """
 通过baostock，获取国内股票数据，并写入mysql
--- data_center.py
+++ data_center.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+# !/usr/bin/python3
 import baostock as bs
 import datetime
 import sys
